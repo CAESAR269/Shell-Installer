ee42007dbc97c3f2d44c9c3bbb17d672bdfd55b5
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-08-25 17:36:22 +0530

    Update kube-user.sh

--- kube-user.sh
+++ kube-user.sh
@@ -1,5 +1,5 @@
-openssl genrsa -out muthu.key 2048
-openssl req -new -key muthu.key -out muthu.csr -subj "/CN=muthu/O=learning"
-openssl x509 -req -in muthu.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out muthu.crt -days 365
-kubectl config set-credentials muthu --client-certificate=muthu.crt --client-key=muthu.key
-kubectl config set-context muthu-context --cluster=kubernetes --user=muthu
+sudo openssl genrsa -out devil.key 2048
+sudo openssl req -new -key devil.key -out devil.csr -subj "/CN=devil/O=learning"
+sudo openssl x509 -req -in devil.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out devil.crt -days 365
+sudo kubectl config set-credentials devil --client-certificate=devil.crt --client-key=devil.key
+sudo kubectl config set-context devil-context --cluster=kubernetes --user=devil
