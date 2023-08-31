8e7a69e834537004dde895a857565a6b6f3b10d6
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-08-31 16:31:28 +0530

    Update kube-user.sh

--- kube-user.sh
+++ kube-user.sh
@@ -2,5 +2,5 @@ name=ram
 sudo openssl genrsa -out $name.key 2048
 sudo openssl req -new -key $name.key -out $name.csr -subj "/CN=$name/O=learning"
 sudo openssl x509 -req -in $name.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out $name.crt -days 365
-sudo kubectl config set-credentials $ --client-certificate=$name.crt --client-key=$name.key
+sudo kubectl config set-credentials $name --client-certificate=$name.crt --client-key=$name.key
 sudo kubectl config set-context $name-context --cluster=kubernetes --user=$name
