d300b1429fe97b5685a872179cc5d576f2e8fc65
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-11-22 21:05:47 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -1,5 +1,5 @@
 az group create --name myResourceGroup --location eastus
 
-az aks create -g myResourceGroup -n myAKSCluster --enable-managed-identity --node-count 1 --node-vm-size Standard_B2s
+az aks create -g myResourceGroup -n myAKSCluster --enable-managed-identity --node-count 1 --node-vm-size Standard_B2s --enable-addons azure-keyvault-secrets-provider
 
 az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
